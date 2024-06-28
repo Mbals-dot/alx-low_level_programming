863d9ca2e49f1396a8b16f26d9ff5c47362c71d0
Mbals-dot <Mskhosana19@gmail.com>
2024-06-28 12:31:19 -0700

    Task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function tht prints if Integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int main(void)
+	i = 0;
+	psitive_or_negative(i);
+
+	return (0);
+}
