d02c5485d8116a5c0433edaa9df269e3851862d9
Mbals-dot <Mskhosana19@gmail.com>
2024-07-02 13:02:19 -0700

    Task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function name
+ * @a: first parameter
+ * @b: seconf parmeter
+ */
+
+void swa-_int (int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b + c;
+}
