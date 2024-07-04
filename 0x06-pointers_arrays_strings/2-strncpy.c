69c3f39ac8cd299d9dadfeb675dfa2b9a0cf2e82
Mbals-dot <Mskhosana19@gmail.com>
2024-07-04 14:21:37 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
-  * _strcmp - The main function
+  * _strncpy - The main function of the code
   *
-  * @s1: Function parameter
+  * @src: Function parameter
   *
-  * @s2: Function parameter
+  * @dest: Function parameter
   *
-  * Return: s1, s2
+  * @n: Function parameter
+  *
+  * Return: dest
   */
-int _strcmp(char *s1, char *s2)
+char *_strncpy(char *dest, char *src, int n)
 {
-int k = 0;
+	int k = 0;
 
-while (s1[k] != '\0' && s2[k] != '\0')
-{
-	if (s1[k] != s2[k])
+	while (k < n && src[k] != '\0')
 	{
-	return (s1[k] - s2[k]);
-	}
+	dest[k] = src[k];
 	k++;
 	}
-	return (0);
+	while  (k < n)
+	{
+	dest[k] = '\0';
+	k++;
+	}
+	return (dest);
 }
